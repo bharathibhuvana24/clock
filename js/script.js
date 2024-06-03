91053b6e733fd820805b81cada54c30a10b59562
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-06-03 11:18:43 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,40 +1,4 @@
-/*const secondHand = document.getElementById("second-hand");
-const minuteHand = document.getElementById("min-hand");
-const hourHand = document.getElementById("hour-hand");
 
-function clockTick()
-{
-    var date = new Date();
-    var seconds = date.getSeconds();
-    var minutes = (seconds + date.getMinutes());
-    var hour = (minutes + date.getHours());
-
-    rotateClockHand(secondHand, seconds);
-    rotateClockHand(minuteHand, minutes);
-    rotateClockHand(hourHand, hour);
-
-}
-
-function rotateClockHand(element, rotation)
-{
-    element.style.setProperty("--rotate", rotation*360);
-
-}
-setInterval(clockTick, 1000); */
-
-/*setInterval(() => {
-    d = new Date(); //object of date()
-    hr = d.getHours();
-    min = d.getMinutes();
-    sec = d.getSeconds();
-    hr_rotation = 30 * hr + min / 2; //converting current time
-    min_rotation = 6 * min;
-    sec_rotation = 6 * sec;
- 
-    hour.style.transform = `rotate(${hr_rotation}deg)`;
-    minute.style.transform = `rotate(${min_rotation}deg)`;
-    second.style.transform = `rotate(${sec_rotation}deg)`;
-}, 1000);*/
 
 
 const secondHand = document.querySelector('.second-hand');
@@ -45,15 +9,15 @@ function setDate() {
   const now = new Date();
    
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const secondsDegrees = ((seconds / 60) * 360) ;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   
   const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6);
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
   
   const hour = now.getHours();
-  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30);
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
